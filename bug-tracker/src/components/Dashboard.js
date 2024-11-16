0dc9f74d4df8e4822d4431ca77cdafcc2af89ea2
bhavani shanker <bhavanishanker@bhavanis-MacBook-Pro.local>
2024-11-16 23:52:08 +0530

    color

--- bug-tracker/src/components/Dashboard.js
+++ bug-tracker/src/components/Dashboard.js
@@ -65,8 +65,8 @@ export default function Dashboard() {
                         <h1 className="text-xl font-bold text-indigo-600 flex items-center">Bug Tracker</h1>
                         <button
                             onClick={handleLogout}
-                            className="inline-flex items-center px-4 py-2 bg-red-500 text-white text-sm font-medium rounded-md hover:bg-red-600"
-                        >
+                            className="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                    >
                             Logout
                         </button>
                     </div>
