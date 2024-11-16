da7ea7f7dfb413dd0cada5870b88e60328fd4f15
bhavani shanker <bhavanishanker@bhavanis-MacBook-Pro.local>
2024-11-16 18:41:07 +0530

    Home Page Added

--- bug-tracker/src/App.js
+++ bug-tracker/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard'; // Import other components
 import LoginPage from './pages/LoginPage';
+import Home from './app/page';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/login' element={<LoginPage/>}></Route>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Home />} />
         <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </Router>
--- bug-tracker/src/app/page.js
+++ bug-tracker/src/app/page.js
@@ -13,3 +13,4 @@ function Home() {
     </div>
   );
 }
+export default Home;
